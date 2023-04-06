ea72ac8249bb4ecd7fe080dd80ef8f4509bdd670
faheem-tekion <fahmad@tekion.com>
2023-04-06 16:23:03 +0530

    feat

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+const port = 4001;
+
+app.use(express.json());
+
+let todos = [
+  { id: 1, title: 'Todo 1', completed: false },
+  { id: 2, title: 'Todo 2', completed: true },
+  { id: 3, title: 'Todo 3', completed: false }
+];
+
+// GET all todos
+app.get('/todos', (req, res) => {
+  res.json(todos);
+});
+
+// GET a specific todo by id
+app.get('/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todos.find(todo => todo.id === id);
+  if (todo) {
+    res.json(todo);
+  } else {
+    res.status(404).send('Todo not found');
+  }
+});
+
+// POST a new todo
+app.post('/todos', (req, res) => {
+  const todo = req.body;
+  todo.id = todos.length + 1;
+  todos.push(todo);
+  res.status(201).json(todo);
+});
+
+// PUT (update) an existing todo by id
+app.put('/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const todoIndex = todos.findIndex(todo => todo.id === id);
+  if (todoIndex !== -1) {
+    const updatedTodo = { ...todos[todoIndex], ...req.body };
+    todos[todoIndex] = updatedTodo;
+    res.json(updatedTodo);
+  } else {
+    res.status(404).send('Todo not found');
+  }
+});
+
+// DELETE a todo by id
+app.delete('/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const todoIndex = todos.findIndex(todo => todo.id === id);
+  if (todoIndex !== -1) {
+    todos.splice(todoIndex, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send('Todo not found');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
